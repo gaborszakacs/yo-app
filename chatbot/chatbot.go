467e1ae3dd18092055a6bcc7db522f333e9f32b6
Gabor <gabor.szakacs@bitrise.io>
2019-06-18 10:15:04 +0200

    Respond to yo-yo

--- chatbot/chatbot.go
+++ chatbot/chatbot.go
@@ -2,5 +2,9 @@ package chatbot
 
 // Respond gives a response to a user input
 func Respond(input string) string {
-	return "yo"
+	if input == "yo-yo" {
+		return "yo-yo"
+	} else {
+		return "yo"
+	}
 }
--- chatbot/chatbot_test.go
+++ chatbot/chatbot_test.go
@@ -15,6 +15,10 @@ func TestRespond(t *testing.T) {
 			input:            "yo",
 			expectedResponse: "yo",
 		},
+		{
+			input:            "yo-yo",
+			expectedResponse: "yo-yo",
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.input, func(t *testing.T) {
